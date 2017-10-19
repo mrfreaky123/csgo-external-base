669c6ea6eb00d9b0466a688bfb2c8958f2dbd5d9
mrfreaky123 <henrycampbell135@gmail.com>
2017-10-19 20:52:36 +1100

    added knife&grenade checks to triggerbot

--- ExternalBase/Utility/Checks.cs
+++ ExternalBase/Utility/Checks.cs
@@ -16,7 +16,7 @@ namespace ExternalBase.Utility
             int team = Mem.Memory.Read<int>(entitybase + Netvars.m_iTeamNum);
             int health = Mem.Memory.Read<int>(entitybase + Netvars.m_iHealth);
 
-            if (team != Structs.LocalPlayer.teamNum && health > 0)
+            if (team != Structs.LocalPlayer.teamNum && health > 0 && ShouldTrigger(Structs.LocalPlayer.WeaponID))
                 return true;
             else
                 return false;
@@ -25,6 +25,13 @@ namespace ExternalBase.Utility
         public static bool IsKeyPushedDown(System.Windows.Forms.Keys vKey)
         {
             return 0 != (Windows.Imports.GetAsyncKeyState(vKey) & 0x8000);
+        }
+
+        public static bool ShouldTrigger(int weaponid)
+        {
+            if (weaponid >= 43 && weaponid <= 48) return false; //grenade check
+            if (weaponid == 42) return false; //knife check
+            return true;
         }
     }
 }
